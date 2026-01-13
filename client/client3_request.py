797f42a549007d34c69df4ba3f660b52b2fe0962
kuremovic22 <162719066+kuremovic22@users.noreply.github.com>
2026-01-13 21:48:23 +0100

    Update SERVER_URL to new IP address

--- client/client3_request.py
+++ client/client3_request.py
@@ -3,7 +3,7 @@ import base64
 from pathlib import Path
 import requests
 
-SERVER_URL = "http://172.16.1.1:8000/encode"
+SERVER_URL = "http://172.16.1.10:8000/encode"
 
 URL = "https://www.techradar.com/phones/android/google-messages-is-getting-a-big-update-in-2026-here-are-the-3-new-whatsapp-style-features-to-look-out-for"
 
