e941026831b632e7afecfe78b037140da3ca9e77
kuremovic22 <162719066+kuremovic22@users.noreply.github.com>
2026-01-13 21:48:06 +0100

    Update SERVER_URL to new IP address

--- client/client2_request.py
+++ client/client2_request.py
@@ -3,7 +3,7 @@ import base64
 from pathlib import Path
 import requests
 
-SERVER_URL = "http://172.16.1.1:8000/encode"
+SERVER_URL = "http://172.16.1.10:8000/encode"
 
 URL = "https://hr.wikipedia.org/wiki/Hrvatska"
 
