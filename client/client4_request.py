de549096b89e7dbd56b6563fef1fa9d1a43bf815
kuremovic22 <162719066+kuremovic22@users.noreply.github.com>
2026-01-13 20:07:12 +0100

    Add client4_request.py to handle URL encoding
    This script sends a URL to a server for encoding and saves the results in specified files.

---
+++ client/client4_request.py
@@ -0,0 +1,26 @@
+import json
+import base64
+from pathlib import Path
+import requests
+
+SERVER_URL = "http://172.16.1.1:8000/encode"
+
+URL = "https://hr.wikipedia.org/wiki/Hrvatska"
+
+client_dir = Path("./client2")
+client_dir.mkdir(parents=True, exist_ok=True)
+
+try:
+    response = requests.post(SERVER_URL, json={"url": URL})
+    response.raise_for_status()
+    data = response.json()
+
+    (client_dir / "original.txt").write_text(data["original_text"], encoding="utf-8")
+    (client_dir / "huffman_codes.json").write_text(json.dumps(data["codes"], indent=2), encoding="utf-8")
+    encoded_bytes = base64.b64decode(data["encoded_b64"])
+    (client_dir / "encoded_data.bin").write_bytes(encoded_bytes)
+
+    print(f"Datoteke za client2 spremljene u {client_dir}")
+
+except Exception as e:
+    print(f"Greška: {e}")
